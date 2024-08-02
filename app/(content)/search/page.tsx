8c29c6fc315228819b1278e5ee3c2302b810050e
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-08-02 20:18:11 +0900

    fix: 빌드에러 수정

--- app/(content)/search/page.tsx
+++ app/(content)/search/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import SearchSection from "@/components/search/SearchSection";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "에피그램 검색",
@@ -7,8 +8,10 @@ export const metadata: Metadata = {
 
 export default function Search() {
   return (
-    <main className="w-[640px] mt-6">
-      <SearchSection />
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="w-[640px] mt-6">
+        <SearchSection />
+      </main>
+    </Suspense>
   );
 }
